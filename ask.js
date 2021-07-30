69711f6f13b89811d446086aa22cde46e117925d
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-30 04:11:14 -0400

    complete seperate of concerns for testing

--- ask.js
+++ ask.js
@@ -1,7 +1,29 @@
-const rl = require("readline");
-const readline = rl.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
 
-const ask = (questionText) =>
-  new Promise((resolve, reject) => readline.question(questionText, resolve));
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
+
+const ask = (prompt) =>
+  new Promise((resolve, reject) => {
+    readlineInterface.question(prompt, resolve);
+  });
+
+// testing code
+// (async () => {
+// This works!
+// ===============
+// let name = await ask("what is your name?\n>_");
+// console.log("your name is", name);
+// process.exit(0);
+// ? attempt two -- doesn't allow me to enter my name -- why?
+// ============
+// ask("what is your name?\n>_")
+//   .then(function (reply) {
+//     console.log("user replied...", reply);
+//   })
+//   .finally(process.exit(0));
+// })();
 
 module.exports = ask;
