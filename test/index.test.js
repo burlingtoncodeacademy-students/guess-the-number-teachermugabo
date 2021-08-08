b6e82359ed66495d4afb611c3eee8b04a78742cb
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-08-08 00:46:51 -0400

    re-add lowerMidpoint tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 // file to test
-const { alternatingMidpoint } = require("../index");
+const { alternatingMidpoint, lowerMidpoint } = require("../index");
 
 // testing libraries
 const assert = require("assert");
@@ -18,10 +18,35 @@ describe("Computer guessing number", () => {
     // sinon.restore();
   });
 
+  it("can find simple lower-mid point", () => {
+    assert.strictEqual(lowerMidpoint(4, 0), 2, "2 is mid (0,4)");
+    assert.strictEqual(lowerMidpoint(3, 0), 1, "1 is lower-mid (0,3)");
+    assert.strictEqual(lowerMidpoint(6, 3), 4, "4 is lower-mid (3,6)");
+    assert.strictEqual(lowerMidpoint(7, 3), 5, "5 is mid (3,7)");
+  });
+
+  it("Can check upper limit when global 'checkedUpperLimit' is false", () => {
+    global.checkedUpperBound = false;
+    assert.strictEqual(
+      lowerMidpoint(7, 6),
+      7,
+      "down to 6 and 7, start with upper bound"
+    );
+  });
+
+  it("Can check upper limit when global 'checkedUpperLimit' is false", () => {
+    global.checkedUpperBound = true;
+    assert.strictEqual(
+      lowerMidpoint(7, 6),
+      6,
+      "down to 6 and 7, having checked upper, now try lower."
+    );
+  });
+
   it("Can find an integer simple mid point", () => {
-    assert.strictEqual(alternatingMidpoint(0, 4), 2, "2 is mid (0,4)");
-    assert.strictEqual(alternatingMidpoint(0, 2), 1, "1 is mid (0,2)");
-    assert.strictEqual(alternatingMidpoint(4, 6), 5, "5 is mid (4,5)");
+    assert.strictEqual(alternatingMidpoint(4, 0), 2, "2 is mid (0,4)");
+    assert.strictEqual(alternatingMidpoint(2, 0), 1, "1 is mid (0,2)");
+    assert.strictEqual(alternatingMidpoint(6, 4), 5, "5 is mid (4,5)");
   });
 
   it("Can find an integer 'alternating' mid point", () => {
