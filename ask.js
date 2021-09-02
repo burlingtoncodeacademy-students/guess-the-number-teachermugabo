612332fcb911b3dac2c9063693d323b83a3021dd
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-09-02 12:18:54 -0400

    clean up ask.js - no functional change

--- ask.js
+++ ask.js
@@ -5,25 +5,17 @@ const readlineInterface = readline.createInterface(
   process.stdout
 );
 
+/**
+ * Name: ask
+ *
+ * Helper method to prompt user input.
+ *
+ * @param {String} prompt for the question
+ * @returns
+ */
 const ask = (prompt) =>
   new Promise((resolve, reject) => {
     readlineInterface.question(prompt, resolve);
   });
-
-// testing code
-// (async () => {
-// This works!
-// ===============
-// let name = await ask("what is your name?\n>_");
-// console.log("your name is", name);
-// process.exit(0);
-// ? attempt two -- doesn't allow me to enter my name -- why?
-// ============
-// ask("what is your name?\n>_")
-//   .then(function (reply) {
-//     console.log("user replied...", reply);
-//   })
-//   .finally(process.exit(0));
-// })();
 
 module.exports = ask;
